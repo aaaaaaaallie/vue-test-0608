52556f353b9d13d6965598feab851843b6a3de78
AllieChen <wei011004@gmail.com>
2022-06-09 17:04:12 +0800

    update1

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <BootstrapCard></BootstrapCard>
+  </div>
+</template>
+
+<script>
+import BootstrapCard from "@/components/BootstrapCard.vue";
+
+export default {
+  components: {
+    BootstrapCard,
+  },
+};
+</script>
